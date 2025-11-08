44959a254f8ffbd36da1966c593c6f6a92e3f633
chenle02 <chenle02@gmail.com>
2025-11-07 22:02:40 -0500

    feat(Keychrone_Q11_Box): add parametric enclosure designs and STL files for left and right enclosures
    Add new OpenSCAD files for creating parametric enclosures with
    outer slip-over lids for both left and right configurations. The
    designs are based on user specifications for inner cavity dimensions
    and wall thickness. STL files for both the bottom and lid parts
    are generated for 3D printing.
    
    The script.sh file is updated to generate STL files for both left
    and right enclosures using OpenSCAD. This change allows for easy
    generation of 3D models for different configurations, enhancing
    the flexibility and usability of the design process.

--- 202511_Keychrone_Q11_Box/script.sh
+++ 202511_Keychrone_Q11_Box/script.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # #!/bin/bash
 # Bottom
-openscad -o Right_bottom.stl -D 'part="bottom"' ./Right_170x140x35_all.scad
+openscad -o Left_bottom.stl -D 'part="bottom"' Left_170x140x35_all.scad
+openscad -o Left_lid.stl -D 'part="lid"' Left_170x140x35_all.scad
 
-# Lid
-openscad -o Right_lid.stl -D 'part="lid"' ./Right_170x140x35_all.scad
+# Bottom
+openscad -o Right_bottom.stl -D 'part="bottom"' Right_190x140x35_all.scad
+openscad -o Right_lid.stl -D 'part="lid"' Right_190x140x35_all.scad
